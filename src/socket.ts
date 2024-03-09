58dd7cdfb6f410520f7a454116a23b7a59772fe5
Christopher Sy Alba <christopher.alba@turners.co.nz>
2024-03-10 12:49:02 +1300

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -4,9 +4,8 @@ let port = "localhost:5000";
 if (!isDev()) {
   port = " https://chris-alba-react-chess-server-42e5afb0e433.herokuapp.com/";
   console.log(port);
-  
 } else {
   console.log(port);
 }
 
-export const socket = io(port);
+export const socket = io(port, { transports: ["websocket"], upgrade: false });
