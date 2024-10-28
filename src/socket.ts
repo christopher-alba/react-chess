e6eea753f34f908af5566a986633dced65f2fdcd
Christopher Sy Alba <christopher.alba@turners.co.nz>
2024-10-28 16:47:46 +1300

    change port for production

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 import isDev from "./process";
 let port = "localhost:5000";
 if (!isDev()) {
-  port = "https://chris-alba-react-chess-server-42e5afb0e433.herokuapp.com";
+  port = "https://react-chess-server.onrender.com";
   console.log(port);
 } else {
   console.log(port);
